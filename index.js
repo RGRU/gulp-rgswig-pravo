31161f47a1bbc7474bcfeb123dd428f2dfc29275
Синецкий Никита <nsinetskiy@rg.net>
2018-12-17 18:26:59 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,405 @@
+/*
+ * Gulp-plugin for custom processing swig templates
+ * @author: rg team
+ *
+ */
+
+var fs = require('fs')
+var path = require('path')
+var es = require('event-stream')
+var gutil = require('gulp-util')
+var _ = require('lodash')
+var minimist = require('minimist')
+
+// Customize extend method LoDash
+var extendify = require('extendify')
+_.extend = extendify({
+  arrays: 'concat'
+})
+
+var swig = require('swig')
+
+// Кастомный фильтр
+// проверяет нахождение элементов в данных,
+// к которым применяем фильтр
+//
+// Аргументы:
+// @input итерируемый элемент,
+// @arrayMask - массив, в котором ищем совпадение
+swig.setFilter('rgInArray', function (input, arrayMask) {
+  var i = 0
+
+  for (; i < arrayMask.length; i++) {
+    if (input === arrayMask[i]) {
+      return true
+    }
+  }
+
+  return false
+})
+
+// Кастомный фильтр
+// получает комментарий из пути файла
+//
+// Аргументы:
+// @_input итерируемый элемент
+swig.setFilter('rgPathComm', function (_input) {
+    return _input ? _input.replace(pathMap.src._, '') : '';
+})
+
+// Кастомный фильтр
+// split
+// разбиваем строку на элементы массива по delimiter
+swig.setFilter('split', function (_input, delimiter) {
+  // Если входящий элемент не строка,
+  // возвращаем его
+  if (typeof _input !== 'string') {
+    return _input
+  }
+
+  return _input.split(delimiter)
+})
+
+// Кастомный фильтр
+// reverse
+// Переопределяем дефолтный фильр,
+// так как он является только алиасом input|sort(true) и делает не совсем то, что ожидается.
+swig.setFilter('reverse', function (_input) {
+  return _input.reverse()
+})
+
+// Кастомный фильтр
+// getRGB
+// подключает RGB блок и отдает его в шаблон
+swig.setFilter('getRGB', function (rgb) {
+  // Подключаем RGB и отдаем данные
+  return require(pathMap._ + '/rgb')(rgb)
+})
+
+// Кастомный фильтр
+// isObject
+// проверяет является ли объектом значение
+// Под объектом понимается любое значение,
+// кроме простого - строка, число, boolean
+swig.setFilter('isObject', function (val) {
+  // Отправляем результат
+  return _.isObject(val)
+})
+
+// Кастомный фильтр
+// merge
+// Объединяем данные
+swig.setFilter('merge', function (_input, mergeData) {
+  // Отправляем результат
+  return _.extend({}, _input, mergeData)
+})
+
+/*
+ * Helpers
+ *
+ */
+
+// If resource exists
+var resExistsSync = function (path) {
+  try {
+    fs.statSync(path)
+    return true
+  } catch (err) {
+    return false
+  }
+}
+
+// Get parent folder
+var parentDir = function (path) {
+  return path.split('/').slice(0, -1).join('/')
+}
+
+// Find template file
+var findTmpl = function (dirPath) {
+  var targetDir = null
+  var tmplFile = 'route.html'
+  var targetFile = null
+
+  // #1 Find setup
+  targetDir = parentDir(dirPath)
+  targetFile = targetDir + '/' + tmplFile
+
+  // #1 Find process
+  if (resExistsSync(targetFile)) {
+    return targetFile
+  } else { // if not file, go to parent dir
+    // #2 Find setup
+    targetDir = parentDir(targetDir)
+    targetFile = targetDir + '/' + tmplFile
+
+    // #2 Find process
+    if (resExistsSync(targetFile)) {
+      return targetFile
+    } else { // if not file, go to default template
+      // #3 Find setup
+      targetDir = parentDir(parentDir(targetDir))
+      targetFile = targetDir + '/' + tmplFile
+
+      // #3 Find process
+      if (resExistsSync(targetFile)) {
+        return targetFile
+      }
+    }
+  }
+
+  return null
+}
+
+// Find crossdata file
+var findCrossData = function (dirPath) {
+  var targetDir = dirPath
+  var crossData = '/crosspages/page.js'
+  var targetFile = null
+
+  // #1 Find setup
+  // ПОЧЕМУ-ТО как глобальная переменная не был объявлен,
+  // если все поломается то из-за этого
+  // !!!!!! ОБРАТИ ВНИМАНИЕ СЮДА ПРИ ОШИБКЕ !!!!!!!
+  targetFile = targetDir + crossData
+
+  // #1 Find process
+  if (resExistsSync(targetFile)) {
+    return targetFile
+  } else { // if not file, go to parent dir
+    // #2 Find setup
+    targetDir = parentDir(parentDir(targetDir))
+    targetFile = targetDir + crossData
+
+    // #2 Find process
+    if (resExistsSync(targetFile)) {
+      return targetFile
+    } else { // if not file, go to default template
+      // #3 Find setup
+      targetDir = parentDir(parentDir(targetDir))
+      targetFile = targetDir + '/data' + crossData
+
+      // #3 Find process
+      if (resExistsSync(targetFile)) {
+        return targetFile
+      }
+    }
+  }
+
+  return null
+}
+
+/*
+ * Module
+ *
+ */
+
+module.exports = function (userOptions) {
+  'use strict'
+
+  /*
+   * Setup
+   *
+   */
+  var
+
+      // default options
+      options = {
+
+        // Custom options for swig
+        swigOpts: {
+          varControls: ['{[', ']}']
+        },
+
+        // Param module
+        param: {
+
+          // Process type
+          compileType: 'tmpl',
+
+          // Ext template
+          extFile: '.html'
+        }
+      }
+
+  // Update options
+  _.merge(options, userOptions)
+
+  // Set swig custom options
+  swig.Swig(options.swigOpts)
+
+  /**
+   *  Set custom filters for swig from {object} options
+   *
+   *  @example
+   *  "filters": [
+   *      {
+   *          "name": 'rgInArray',                        // filter name
+   *          "handler": function (input, arrayMask) {    // realization of filter
+   *              var i = 0
+   *
+   *              for (; i < arrayMask.length; i++) {
+   *                  if (input === arrayMask[i]) {
+   *                      return true
+   *                  }
+   *              }
+   *
+   *              return false
+   *
+   *          }
+   *      },
+   *  ]
+   */
+  if (Array.isArray(options.filters) && options.filters.length) {
+    options.filters.forEach(function (filter, index) {
+      if (filter.name && filter.handler) {
+        swig.setFilter(filter.name, filter.handler)
+      }
+    })
+  }
+
+  // Data processing
+  //      @file - file pass gulp
+  //      @callback - process function
+
+  var rgswig = function (file, callback) {
+    // File contents
+    var fileContents = file.contents
+
+    // Inline style data
+    var styleData = null
+
+    // File path
+    var filePath = file.path
+
+    // src dest
+    var dirPath = null
+
+    // Compile type
+    var compileType = options.param.compileType
+
+    // Ext file
+    var extFile = options.param.extFile
+
+    /*
+     * Template var
+     *
+     */
+
+    // Template
+    var tmpl = null
+
+    // Template data
+    var tmplData = (options.data) ? options.data : null
+
+    // Template cross data
+    var tmplCrossData = null
+
+    // Compiled template
+    var compiled = null
+
+    // Node Arguments
+    var nodeArgv = null
+
+    // Environment
+    var processEnv = null
+
+    // Use custom minify (for current xml minify)
+    var customHtmlMinify = (options.customHtmlMinify) ? true : false
+
+    // Processing
+    try {
+      // Get node arguments
+      nodeArgv = minimist(process.argv)
+
+      // Set Environment
+      processEnv = nodeArgv.env || 'dev'
+
+      // Set ENV as global
+      global.env = processEnv
+
+      // If process from template
+      if (compileType === 'tmpl') {
+        // Compile template file
+        tmpl = swig.compile(
+          String(fileContents),
+          {
+            filename: filePath
+          }
+        )
+      } else if (compileType === 'data') { // If process from data
+        // Store dir path file, when watch gulp
+        dirPath = path.dirname(filePath)
+
+        // Compile template file
+        tmpl = swig.compileFile(findTmpl(dirPath))
+
+        // Set crossdata file
+        tmplCrossData = require(findCrossData(dirPath))
+
+        // Merge data template
+        tmplData = _.extend({}, tmplCrossData, require(filePath).toMerge)
+
+        // Add Environment in data template
+        tmplData = _.extend({}, tmplData, { env: processEnv })
+
+        // fs.writeFile('/www/app/branches/auto/temp/data.js', JSON.stringify(tmplData, false, '\t'), function(err) {
+
+        //     if (err) {
+        //         return console.log(err)
+        //     }
+
+        //     console.log("The file data was saved!")
+
+        // })
+
+        try {
+          // Get style data
+          styleData = JSON.parse(String(file.contents)).styleData
+
+          // if sass data is exist
+          if (!!styleData) {
+            // Add Sass data
+            tmplData = _.extend({}, tmplData, { styleData: styleData })
+          }
+        } catch (err) {
+          console.log(file.path, 'sass error. it is ok :)')
+
+          // fs.writeFile('/www/app/branches/css/temp/errors/sassError' + Math.random().toFixed(5)*100000 + '.js', JSON.stringify(file.path, false, '\t'), function(err) {
+
+          //     if (err) {
+          //         return console.log(err)
+          //     }
+
+          //     console.log("The file data was saved!")
+
+          // })
+        }
+
+        // Set extension
+        file.path = gutil.replaceExtension(filePath, extFile)
+      } else {
+        throw Error('неверно задан тип компиляции')
+      }
+
+      // Compile template,
+      // Whitch customMinify or not
+      if (customHtmlMinify) {
+        compiled = tmpl(tmplData).replace(/\r|\n}/g, '').replace(/\t{1,}|\s{2,}/g, ' ')
+      } else {
+        compiled = tmpl(tmplData)
+      }
+
+      // Save data
+      file.contents = new Buffer(compiled)
+
+      // Send data
+      callback(null, file)
+    } catch (err) {
+      // Send error
+      callback(err)
+    }
+  }
+
+  // Return data
+  return es.map(rgswig)
+}
